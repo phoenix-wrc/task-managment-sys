6fcf84c93014e8131810ef62184affd480f50688
Kirill Novozhilov <phoenix-wrc@yandex.ru>
2024-08-03 14:12:44 +1000

    Init
    Init commit. Create via spring.start.

---
+++ src/main/java/site/ph0en1x/task_managment_sys/TaskManagmentSysApplication.java
@@ -0,0 +1,13 @@
+package site.ph0en1x.task_managment_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagmentSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagmentSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/ph0en1x/task_managment_sys/TaskManagmentSysApplicationTests.java
@@ -0,0 +1,13 @@
+package site.ph0en1x.task_managment_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagmentSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
