cdc8d4d74948f1cde430bc2fb164c531c8496dac
Kirill Novozhilov <phoenix-wrc@yandex.ru>
2024-08-06 22:55:58 +1000

    Ппоравил таски

--- src/main/java/site/ph0en1x/task_management_sys/model/task/TaskDto.java
+++ src/main/java/site/ph0en1x/task_management_sys/model/task/TaskDto.java
@@ -6,8 +6,6 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.validator.constraints.Length;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.format.annotation.DateTimeFormat;
 import site.ph0en1x.task_management_sys.model.comment.CommentDTO;
 import site.ph0en1x.task_management_sys.web.validation.onCreate;
@@ -31,15 +29,17 @@ public class TaskDto {
     private String title;
 
     @Schema(description = "Task description", example = "Try new recipe of spiderweb")
+    @Length(max = 1024, message = "Description must be less than 1024 symbols", groups = {onCreate.class, onUpdate.class})
     private String description;
 
     @Schema(description = "Task status can be  PENDING, IN_PROGRESS, COMPLETED",
             example = "PENDING, IN_PROGRESS, COMPLETED")
+    @NotNull(message = "Status must exist", groups = {onCreate.class, onUpdate.class})
     private TaskStatus status;
 
     @Schema(description = "Task priority can be HIGH, MEDIUM , LOW", example = "HIGH, MEDIUM, LOW")
+    @NotNull(message = "Priority must exist", groups = {onCreate.class, onUpdate.class})
     private TaskPriority priority;
-
 
     @Schema(description = "Task create date in format yyyy-MM-dd HH:mm",
             example = "2025-06-01 11:22", format = "date-time")
@@ -55,16 +55,11 @@ public class TaskDto {
     private LocalDateTime updatedAt;
 
     @Schema(description = "Author ID", example = "3")
-    @NotNull(message = "Must be define")
     private Long authorId;
 
-    @Schema(description = "Assignee user ID", example = "3")
+    @Schema(description = "Assignee user ID. Define as author id as default.", example = "3")
     private Long assigneeId;
 
     @Schema(description = "All comments to task")
     private Set<CommentDTO> comments;
-
-    public TaskDto() {
-        log.debug("TaskDto Constructor");
-    }
 }
--- src/main/java/site/ph0en1x/task_management_sys/service/TaskService.java
+++ src/main/java/site/ph0en1x/task_management_sys/service/TaskService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import site.ph0en1x.task_management_sys.model.exception.ResourceMappingException;
 import site.ph0en1x.task_management_sys.model.exception.ResourceNotFoundException;
 import site.ph0en1x.task_management_sys.model.task.Task;
 import site.ph0en1x.task_management_sys.repository.TaskRepository;
@@ -20,28 +21,28 @@ public class TaskService {
 
     public Task createTask(Task task) {
         task.setCreatedAt(LocalDateTime.now());
+        task.setId(null);
         return taskRepository.save(task);
     }
 
     public Task updateTask(Task task) {
+        if (!taskRepository.existsById(task.getId())) {
+            throw new ResourceNotFoundException("Task with that ID not found");
+        }
         return taskRepository.save(task);
     }
 
     public void deleteTask(Long id) {
+        if (!taskRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Task with that ID not found");
+        }
         taskRepository.deleteById(id);
     }
 
     public Task getTask(Long id) {
-        log.debug("Into getTask.Service with id: {}", id);
-        Task out;
-        try {
-            out = taskRepository.findById(id).orElseThrow(() ->
-                    new ResourceNotFoundException("Задача с таким ID не найдена в базе данных"));
-        } catch (MethodArgumentTypeMismatchException ex) {
-            throw new ResourceNotFoundException(ex.getMessage() + " " + ex.getName());
-        }
-        log.debug("Get Task: {}", out);
-        return out;
+        log.debug("Get Task: {}", id);
+        return taskRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Задача с таким ID не найдена в базе данных"));
     }
 
     public List<Task> getTasksByAssigneeId(Long assigneeId) {
